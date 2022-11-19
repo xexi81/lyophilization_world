b38fd9bf6fd8d23c96ebdab4819044ffb2c75235
Sergio Núñez <sergionunez90@gmail.com>
2022-11-19 12:15:38 +0100

    Crashlytics

--- app/src/main/java/com/los3molineros/lyophilization_world/ui/MainActivity.kt
+++ app/src/main/java/com/los3molineros/lyophilization_world/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.los3molineros.lyophilization_world
+package com.los3molineros.lyophilization_world.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    throw RuntimeException("Test Crash")
                     Greeting("Android")
                 }
             }
---
+++ app/src/main/java/com/los3molineros/lyophilization_world/ui/composables/Icon.kt
@@ -0,0 +1,8 @@
+package com.los3molineros.lyophilization_world.ui.composables
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Icon() {
+
+}

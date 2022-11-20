62dcf21b9271f5b4ebedca34ae953846102d61a7
Sergio Núñez <sergionunez90@gmail.com>
2022-11-20 23:07:02 +0100

    Iconos finalizado

--- app/src/main/java/com/los3molineros/lyophilization_world/ui/composables/IconApp.kt
+++ app/src/main/java/com/los3molineros/lyophilization_world/ui/composables/IconApp.kt
@@ -2,17 +2,11 @@ package com.los3molineros.lyophilization_world.ui.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,40 +25,76 @@ fun IconApp(
     tint: Color = Color.Transparent,
     backgroundColor: Color = Color.Transparent,
     isCircleShape: Boolean = false,
+    isBadgeBox: Boolean = false,
+    badgeText: String = "",
     iconClicked: () -> Unit = {}
 ) {
-    IconButton(
-        modifier = Modifier
-            .size(size).padding(1.dp)
-            .then(
-                if (isCircleShape)
-                    Modifier
-                        .border(width = 0.dp, color = Color.Gray, shape = CircleShape)
-                        .background(backgroundColor, shape = CircleShape)
-                else Modifier
-            ),
-        onClick = iconClicked,
-        content =
-        {
-            if (imageVector != null)
-            Icon(
-                modifier = Modifier.size(size),
-                imageVector = imageVector,
-                contentDescription = null,
-                tint = tint,
-            )
-            else if (resource != null)
-                Icon(
-                    modifier = Modifier.size(size),
-                    painter = painterResource(resource),
-                    contentDescription = null,
-                    tint = tint,
-                )
-        }
-    )
+    if (isBadgeBox) {
+        BadgedBox(
+            badge = { Badge { Text(badgeText) } },
+            content = { ImageIcon(imageVector, resource, size, tint)} ,
+            modifier = Modifier.clickable { iconClicked() }
+        )
+    } else
+    {
+        IconButton(
+            modifier = Modifier
+                .size(size)
+                .padding(1.dp)
+                .then(
+                    if (isCircleShape)
+                        Modifier
+                            .border(
+                                width = 0.dp,
+                                color = Color.Gray,
+                                shape = CircleShape)
+                            .background(
+                                backgroundColor,
+                                shape = CircleShape)
+                    else Modifier
+                ),
+            onClick = { iconClicked() },
+            content = { ImageIcon(imageVector, resource, size, tint) }
+        )
+    }
 }
 
+@Composable
+private fun ImageIcon(
+    imageVector: ImageVector? =  null,
+    resource: Int? = null,
+    size: Dp = 50.dp,
+    tint: Color = Color.Transparent
+) {
+    if (imageVector != null)
+        Icon(
+            modifier = Modifier.size(size),
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = tint,
+        )
+    else if (resource != null)
+        Icon(
+            modifier = Modifier.size(size),
+            painter = painterResource(resource),
+            contentDescription = null,
+            tint = tint,
+        )
+}
 
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun PostComments() {
+    IconApp(
+        imageVector = null,
+        resource = R.drawable.comment,
+        size = 70.dp,
+        tint = Color.Black,
+        isCircleShape = false,
+        isBadgeBox = true,
+        iconClicked = {}
+    )
+}
 
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
@@ -116,20 +146,6 @@ fun EditPost() {
         resource = R.drawable.pen,
         size = 70.dp,
         tint = Color.Gray,
-        isCircleShape = false,
-        iconClicked = {}
-    )
-}
-
-
-@Preview(showBackground = true, showSystemUi = false)
-@Composable
-fun PostComments() {
-    IconApp(
-        imageVector = null,
-        resource = R.drawable.comment,
-        size = 70.dp,
-        tint = Color.Black,
         isCircleShape = false,
         iconClicked = {}
     )
